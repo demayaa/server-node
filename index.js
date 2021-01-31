2d5b46d448b2ecb688f35acebf16db17b016d4c1
demayaa <demayaa45@gmail.com>
2021-01-31 11:39:01 +0700

    node server start

---
+++ index.js
@@ -0,0 +1,13 @@
+const ganache = require("ganache-cli");
+const server = ganache.server({
+    mnemonic: "cherry garden lava garden shop vast lava lava vast album royal royal",
+    default_balance_ether: 4000000000000000,
+    total_accounts: 1,
+    network_id: 3324,
+    _chainId: 109,
+    _chainIdRpc: 1332,
+    defaultTransactionGasLimit: '0x99f90000'
+});
+server.listen(8454, function(err, blockchain) {
+    console.log('server connect')
+});
